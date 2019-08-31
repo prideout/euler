cf017f24b944aa748ed466e4d80ba76d450e5f93
Philip Rideout <philiprideout@gmail.com>
2019-08-31 10:08:27 -0700

    Use bmp suffix to trick nginx for gzip.

--- src/urls.ts
+++ src/urls.ts
@@ -3,12 +3,12 @@ const environ = "env/pillars";
 export const ibl = `${environ}_ibl.ktx.bmp`;
 export const sky = `${environ}_skybox.ktx.bmp`;
 
-export const step1Material = "materials/step1.filamat";
-export const step1CylinderBackMaterial = "materials/step1_cylinder_back.filamat";
-export const step1CylinderFrontMaterial = "materials/step1_cylinder_front.filamat";
-export const step2Material = "materials/step2.filamat";
-export const step3Material = "materials/step3.filamat";
-export const step4Material = "materials/step4.filamat";
+export const step1Material = "materials/step1.filamat.bmp";
+export const step1CylinderBackMaterial = "materials/step1_cylinder_back.filamat.bmp";
+export const step1CylinderFrontMaterial = "materials/step1_cylinder_front.filamat.bmp";
+export const step2Material = "materials/step2.filamat.bmp";
+export const step3Material = "materials/step3.filamat.bmp";
+export const step4Material = "materials/step4.filamat.bmp";
 
 export const diffuse = "diffuse.jpg";
 export const specular = "specular.jpg";
